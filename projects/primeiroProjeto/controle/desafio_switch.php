8791b06529ba79a930cd44b41a0a4fb2ee83b0d2
Jean Melo <jeanmamelo@gmail.com>
2021-01-06 10:14:14 -0300

    updated 'desafio_switch.php'

--- projects/primeiroProjeto/controle/desafio_switch.php
+++ projects/primeiroProjeto/controle/desafio_switch.php
@@ -3,10 +3,12 @@
 <form action="#" method="post">
     <input type="text" name="param">
     <select name="conversao" id="conversao">
-        <option value="km-milha">1 KM => 0.621371 Milhas</option>
-        <option value="milha-km">1 Milha => 1.60934 KM</option>
-        <option value="metro-km">1 Metros => 0.001 KM</option>
-        <option value="km-metro">1 KM => 1000 Metros</option>
+        <option value="km-milha">KM => Milha</option>
+        <option value="milha-km">Milha => KM</option>
+        <option value="metro-km">Metro => KM</option>
+        <option value="km-metro">KM => Metro</option>
+        <option value="f-c">°F => °C</option>
+        <option value="c-f">°C => °F</option>
     </select>
     <button>Calcular</button>
 </form>
@@ -25,7 +27,7 @@ echo "<p class='divisao'>Minha resposta</p><hr>";
 $input = $_POST['param'];
 $conversao = $_POST['conversao'];
 
-if ($input) {
+if (isset($input)) {
     switch (strtolower($conversao)) {
         case 'km-milha':
             $conversao = $input * 0.621371;
@@ -55,6 +57,20 @@ if ($input) {
             
             echo "<br>$input $unidadeInput = $conversao $unidadeConversao";
             break;
+        case 'f-c':
+            $conversao = (($input - 32) * 5) / 9;
+            $unidadeInput = "fahrenheit";
+            $unidadeConversao = "celsius";
+            
+            echo "<br>{$input}° $unidadeInput = {$conversao}° $unidadeConversao";
+            break;
+        case 'c-f':
+            $conversao = (($input * 9) / 5) + 32;
+            $unidadeInput = "celsius";
+            $unidadeConversao = "fahrenheit";
+            
+            echo "<br>{$input}° $unidadeInput = {$conversao}° $unidadeConversao";
+            break;
         default:
             "<br>Digite um número válido para ser convertido";
     }
@@ -66,23 +82,36 @@ if ($input) {
 echo "<p class='divisao'>Resposta do professor</p><hr>";
 const FATOR_KM_MILHA = 0.621371;
 const FATOR_METRO_KM = 1000;
+const FATOR_CEL_FAH = 1.8;
 
 $param = $_POST['param'] ?? 0;
 switch ($_POST['conversao']) {
     case 'km-milha':
         $distancia = $param * FATOR_KM_MILHA;
-        $mensagem = "$param KM = $distancia milhas";
+        $mensagem = "$param KM(s) = $distancia milha(s)";
         break;
     case 'milha-km':
         $distancia = $param / FATOR_KM_MILHA;
-        $mensagem = "$param milhas = $distancia KM";
+        $mensagem = "$param milha(s) = $distancia KM(s)";
         break;
     case 'metro-km':
-
+        $distancia = $param / FATOR_METRO_KM;
+        $mensagem = "$param metro(s) = $distancia KM(s)";
         break;
     case 'km-metro':
-
+        $distancia = $param * FATOR_METRO_KM;
+        $mensagem = "$param KM(s) = $distancia metro(s)";
+        break;
+    case 'f-c':
+        $conversao = ($param - 32) / FATOR_CEL_FAH;
+        $mensagem = "{$param}° fahrenheit = {$conversao}° celsius";
+        break;
+    case 'c-f':
+        $conversao = $param * FATOR_CEL_FAH + 32;
+        $mensagem = "{$param}° celsius = {$conversao}° fahrenheit";
         break;
     default:
-        "<br>Digite um número válido para ser convertido";
+        $mensagem = "Nenhum valor calculado!";
 }
+
+echo "<p>$mensagem</p>";
