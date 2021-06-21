7ac6f6710be63412a19ae95a6de0931ed7f98df8
Jean Melo <jeanmamelo@gmail.com>
2021-06-21 11:09:05 -0300

    add 'traits_02.php'

--- projects/primeiroProjeto/index.php
+++ projects/primeiroProjeto/index.php
@@ -53,6 +53,9 @@
                         <li>
                             <a href="exercicio.php?dir=classes_objetos&file=traits_01">Traits #01</a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=classes_objetos&file=traits_02">Traits #02</a>
+                        </li>
                     </ul>
                 </div>
                 <div class="modulo vermelho-escuro">
